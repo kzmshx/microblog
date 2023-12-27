1fbc5946e1d9a58cca3fb5f4d00d0c758977c3b9
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-27 22:38:39 +0900

    apps/learn-ui-testing: Refactor

--- apps/learn-ui-testing/__tests__/components/Button.test.tsx
+++ apps/learn-ui-testing/__tests__/components/Button.test.tsx
@@ -1,12 +1,12 @@
 import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Button from "../../app/components/Button";
 
 describe("Button", () => {
   it("should render a button with the given label", () => {
-    render(<Button label="ボタン" onClick={() => alert("click")} />);
+    const { getByRole } = render(<Button label="ボタン" onClick={() => alert("click")} />);
 
-    const element = screen.getByRole("button");
+    const element = getByRole("button");
     expect(element).toBeInTheDocument();
     expect(element).toHaveTextContent("ボタン");
   });
