46c3150833bd051249c8f3adad9ed990c9988bed
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-27 21:39:40 +0900

    apps/learn-ui-testing: Format

--- apps/learn-ui-testing/app/components/Form.tsx
+++ apps/learn-ui-testing/app/components/Form.tsx
@@ -10,12 +10,7 @@ const Form = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Enter text"
-      />
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter text" />
       <button type="submit">Submit</button>
     </form>
   );
