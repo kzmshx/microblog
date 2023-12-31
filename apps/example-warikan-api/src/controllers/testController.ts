53265e1c90c2a21ed6013435deb488dde59ca1d8
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-31 22:34:30 +0900

    apps/example-warikan-api: Fix TestController

--- apps/example-warikan-api/src/controllers/testController.ts
+++ apps/example-warikan-api/src/controllers/testController.ts
@@ -2,7 +2,7 @@ import express from "express";
 import fs from "fs";
 
 const GROUP_FILE_PATH = `${process.env.DATA_PATH}/groups.json`;
-const EXPENSE_FILE_PATH = `${process.env.DATA_PATH}//expenses.json`;
+const EXPENSE_FILE_PATH = `${process.env.DATA_PATH}/expenses.json`;
 
 export class TestController {
   init = (req: express.Request, res: express.Response, next: express.NextFunction) => {
