0903e1a0076878eb74ca51fb8ec270b8ce07094b
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-01-05 17:24:22 +0900

    apps/learn-tailwind: Adjust root page style

--- apps/learn-tailwind/app/page.tsx
+++ apps/learn-tailwind/app/page.tsx
@@ -4,15 +4,33 @@ export default function Home() {
       title: "Email Subscribe Card",
       href: "/email-subscribe",
     },
+    {
+      title: "Pricing Cards",
+      href: "/pricing-cards",
+    },
+    {
+      title: "Product Modal",
+      href: "/product-modal",
+    },
+    {
+      title: "Image Gallery",
+      href: "/image-gallery",
+    },
+    {
+      title: "Login Modal",
+      href: "/login-modal",
+    },
   ];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl">Learn Tailwind</h1>
-      <ul className="my-10 ">
-        {contents.map((content) => (
+      <h1 className="text-5xl">Learn Tailwind</h1>
+      <ul className="my-10 flex flex-col space-y-5">
+        {contents.map((content, index) => (
           <li key={content.href} className="transform duration-200 hover:text-zinc-400">
-            <a href={content.href}>{content.title}</a>
+            <a className="text-lg" href={content.href}>
+              {index + 1}. {content.title}
+            </a>
           </li>
         ))}
       </ul>
