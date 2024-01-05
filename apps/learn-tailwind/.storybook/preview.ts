37027bca81ac40744a576a26f8b3049872513cd6
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-01-05 15:45:45 +0900

    apps/learn-tailwind: Support various devices using @storybook/addon-viewport

--- apps/learn-tailwind/.storybook/preview.ts
+++ apps/learn-tailwind/.storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from "@storybook/react";
 import "../app/globals.css";
+import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 const preview: Preview = {
   parameters: {
@@ -12,6 +13,13 @@ const preview: Preview = {
     },
     // Center components in the preview
     layout: "centered",
+    viewport: {
+      viewports: {
+        // Support various devices using @storybook/addon-viewport
+        ...MINIMAL_VIEWPORTS,
+        ...INITIAL_VIEWPORTS,
+      },
+    },
   },
 };
 
